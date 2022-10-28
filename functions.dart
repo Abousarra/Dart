00ee2402a7076ea83f2850f3bac25fce7139e946
El Ghazali <agep2024@gmail.com>
2022-10-28 10:13:27 +0000

    28102022

--- functions.dart
+++ functions.dart
@@ -12,15 +12,37 @@ Function lemdaFun = (x, y) {
   return x - y;
 };
 
-//Dart Lexical Closures fun in fun
+//Dart Lexical Closures : fun in fun
 
 lexicalFun(int x) {
   var c = 15;
-  print(c);
+  print('var lexicalFun $c');
   insideFun() {
     c = c + x;
-    print(c);
+    print('insideFun lexicalFun $c');
   }
 
   return insideFun();
+}
+
+// Dart Recursive Function
+
+recursiveFun(x) {
+  if (x > 0) {
+    return x * recursiveFun(x - 1);
+  } else {
+    return 1;
+  }
+}
+
+// 28.28.  Dart Optional Positional Parameters
+
+paramOptionalFun(x, y, [z = 0, w = 0]) {
+  return x + y + z + w;
+}
+
+// 29.29.  Dart Optional Named Parameters
+
+paramNamedFun({x = 0, y = 0}) {
+  return x + y;
 }
--- main.dart
+++ main.dart
@@ -1,8 +1,14 @@
 import 'functions.dart';
 
 main() {
-  print(normalFun(14, 15));
-  print(fatArrowFun(15.5, 50));
-  print(lemdaFun(15, 5));
-  lexicalFun(15);
+  print('normalFun ${normalFun(14, 15)}');
+  print('fatArrowFun ${fatArrowFun(15.5, 50)}');
+  print('lemdaFun ${lemdaFun(15, 5)}');
+  print('lexicalFun ${lexicalFun(15)}');
+  print('recursiveFun ${recursiveFun(5)}');
+  print('paramOptionalFun ${paramOptionalFun(5, 7)}');
+  print('paramOptionalFun ${paramOptionalFun(5, 7, 12)}');
+  print('paramOptionalFun ${paramOptionalFun(5, 7, 12, 24)}');
+  print('paramNamedFun ${paramNamedFun(x: 5)}');
+  print('paramNamedFun ${paramNamedFun(x: 5, y: 45)}');
 }
